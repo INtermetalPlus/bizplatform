47c20265442d1a738908f73c463c443fcf4aab83
Rustam000 <61691677+Rustam000@users.noreply.github.com>
2024-04-07 18:12:38 +0300

    company info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import styles from "./page.module.css";
+import { Company } from "@/widgets/Company/Company";
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    
+    <Company/>
     </main>
   );
 }
