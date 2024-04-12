67002f2b9b665247f526547542c3ac0ad9ed1758
Abglaa <Altymysbaevabdulaziz@gmail.com>
2024-04-12 22:48:36 +0600

    создал три компонента menu,contact и card

--- src/widgets/Card/Card.tsx
+++ src/widgets/Card/Card.tsx
@@ -1,7 +1,15 @@
 import './../../app/globals.css'
+import styles from "./Menu.module.scss"
+
 
 export default function Card() {
     return (
-        <div></div>
+        <div className={styles.card}>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
     );
 }
