5df20f7bbaf737535b7f0427bf3f8daa0377e864
Abglaa <Altymysbaevabdulaziz@gmail.com>
2024-04-19 18:45:19 +0600

    fix

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+import {Main} from "next/document";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+
+    </main>
+  );
+}
