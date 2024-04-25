849f4283574dfa1e23abe3a69d0c6b200b531dd5
Pavel <hvan1998@inbox.ru>
2024-04-25 17:24:08 +0600

    refactor(chat layout): fixed layout to display properly put page chat in about platform menu temporarily

--- src/pages/Chats/Chats.tsx
+++ src/pages/Chats/Chats.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import '../../app/globals.module.css'
-import styles from './Chats.module.scss'
-
-import { Navigation } from "@/widgets/Header/navigation";
-import { Footer } from "@/widgets/Footer";
 import { ChatsHeader } from "@/shared/ui/Chats/ChatsHeader";
 import { ChatBlock } from "@/widgets/ChatBlock/ChatBlock";
-
+import styles from './Chats.module.scss'
 
 export const Chats: React.FC = () => {
     return (
         <>
-            <div className="container">
-                <Navigation/>
+            <div className={styles.container}>     
                 <ChatsHeader/>
                 <ChatBlock/>
-                <Footer/>
             </div>
         </>
     )
