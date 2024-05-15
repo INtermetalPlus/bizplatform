e171857d3a233c1d1ec7a86ddf667918e31952c7
Helluva-Makets <malcevartem808@gmail.com>
2024-05-15 21:31:47 +0300

    fixed SetCountry component

--- src/shared/ui/GoodsAndServices/SetCountry.tsx
+++ src/shared/ui/GoodsAndServices/SetCountry.tsx
@@ -1,11 +1,10 @@
 'use client'
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import styles from './GAS_shared.module.scss'
 import { GAS_orderStore } from "@/entities/GAS_orderStore/GAS_orderStore";
 
 
 export const GAS_SetCountry: React.FC = () => {
-    const [isList,setIsList] = useState(false)
     const {country, fetchCountry} = GAS_orderStore(state => ({
         country: state.countries,
         fetchCountry: state.fetchCountry
@@ -17,34 +16,6 @@ export const GAS_SetCountry: React.FC = () => {
 
     return (
         <>
-            {/* {country.map((item) => (
-                <div key={item.id} className={styles.setCountry}>
-                    <div 
-                    className={styles.setCountry__firstCountry}
-                    onClick={() => console.log('opened')}
-                    >
-                        <span>{item.country_name}</span>
-                    </div>
-                    {isList && (
-                        <div className={styles.setCountry__list} onClick={() => console.log('opened')}>
-                            <div 
-                            key={item.id} 
-                            className={styles.setCountry__firstCountry}
-                            onClick={() => console.log('opened')}
-                            >
-                                <span>{item.country_name}</span>
-                            </div>
-                            <div 
-                            key={item.id} 
-                            className={styles.setCountry__firstCountry}
-                            onClick={() => console.log('opened')}
-                            >
-                                <span>{item.country_name}</span>
-                            </div>
-                        </div>
-                    )}
-                </div>
-            ))} */}
             <label className={styles.setCountry}>
                 <select name="selectedCountry" className={styles.setCountrylist}>
                     {country.map((item) => (
