1d01138565f166372dc51341bd244f3d675d351f
Helluva-Makets <malcevartem808@gmail.com>
2024-05-25 22:38:48 +0300

    add orders on GAS list page

--- src/features/storage/GAS_listOrders/GAS_orders.ts
+++ src/features/storage/GAS_listOrders/GAS_orders.ts
@@ -21,7 +21,11 @@ interface OrderAPI {
 export const orderListDataHook = create<OrderAPI>((set) => ({
     orders: [],
     fetchOrder: async () => {
-        const orderRequest = await axios.get('http://167.172.161.102:82/api/v1/orders/')
+        const orderRequest = await axios.get('http://167.172.161.102:82/api/v1/orders/', {
+            headers: {
+                Authorization: `JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjo3NzE0NjQwNTE4LCJpYXQiOjE3MTQ2NDA1MDAsImp0aSI6ImE4NjUzYzlhOGRjNTQ0YTM4Yzk3NDNmNzU2NzljOGMyIiwidXNlcl9pZCI6MTB9.5zNV6f2gtSJcxg6GpxS5AcXtfFsWacQqEdpu01yNJdA`
+            }
+        })
         set({orders: orderRequest.data})
     }
 }))
--- src/widgets/ProductList/ProductList.tsx
+++ src/widgets/ProductList/ProductList.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import '../../app/globalStyle.css'
+import './antdSelector_ui.scss'
 import styles from './ProductList.module.scss'
 import { SearchField } from "@/features/searchField";
 import Image from "next/image";
-import Strelka from "../../shared/assets/icons/arrow_forward_ios (1).svg";
+import cross from "../../shared/assets/icons/x-close.png";
 import { CreateOrderModal } from "../CreateOrderModal";
 import { Select } from "antd";
 import SelectIcon from "../../shared/assets/icons/Frame 220.png";
 import { orderListDataHook } from "@/features/storage/GAS_listOrders/GAS_orders";
+import { useOrderFrames } from "@/features/lib/helpers/ProductList_orderHook";
 
 const { Option } = Select;
 
@@ -36,7 +39,8 @@ export const ProductList: React.FC = () => {
     { value: 8, label: 'Инженерные Системы' },
   ];
   
-    
+
+const {isFrame, closeFrame} = useOrderFrames()
 const {orders, fetchOrder} = orderListDataHook()
 
   useEffect(() => {
@@ -50,11 +54,9 @@ const {orders, fetchOrder} = orderListDataHook()
           <div className={styles.categories}>
             <div className={styles.selectDiv}>
               <Select
-                suffixIcon={null}
-                className={styles.select}
-                defaultValue={optionsPlacegolder}
-                optionLabelProp="label"
-                variant="borderless"
+              className={styles.select}
+              defaultValue={optionsPlacegolder}
+              optionLabelProp="label"
               >
               {options.map((option) => (
                 <Option key={option.label} optionFontSize={22}>
@@ -70,10 +72,6 @@ const {orders, fetchOrder} = orderListDataHook()
                 </Option>
               ))}
               </Select>
-              <div className={styles.icon}>
-                {" "}
-                <Image src={Strelka} alt="strelka" width={20} height={20} />
-              </div>
               <div className={styles.BlogCheck}>
               <div className={styles.title}>Наличие</div>
               <div className={styles.viewedCheck}>
@@ -93,7 +91,7 @@ const {orders, fetchOrder} = orderListDataHook()
               </div>
               </div>
               <div className={styles.completedCheck}>
-                  Безналичный расчет{" "}
+                  Безналичный расчет
                 <input type="checkbox" className={styles.check} />
               </div>
               <div className={styles.BlogCheck}>
@@ -112,11 +110,11 @@ const {orders, fetchOrder} = orderListDataHook()
                 <input type="text" placeholder="0 сом" />
                 <div>
                   <svg
-                    width="8"
-                    height="2"
-                    viewBox="0 0 8 2"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
+                  width="8"
+                  height="2"
+                  viewBox="0 0 8 2"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                   >
                     <rect width="8" height="2" fill="#D9D9D9" />
                   </svg>
@@ -146,20 +144,36 @@ const {orders, fetchOrder} = orderListDataHook()
               </button>
               {isOrderModalOpen && <CreateOrderModal onClose={handleCloseOrderModal} />}
             </div>
-            {orders.map((item) => (
-              <div key={item.id} className={styles.order_frame}>
-                <h1>{item.order_title}</h1>
-                <h2>{item.price}</h2>
-                <span>{item.order_text}</span>
-                <span>
-                  Место отправки{item.regions}
-                </span>
-                <span>
-                  Контакты: {item.phone_number}
-                </span>
-                <button type="submit"  className={styles.order_frame__btn}>Написать поставщику</button>
+            <div className={styles.orders}>
+              <div className={styles.orders__frame_block}>
+              {isFrame && 
+                (orders.map((item) => (
+                  <div key={item.id} className={styles.order_frame}>
+                    <div className={styles.order_frame__order_img}>
+                      <Image
+                      src={cross}
+                      width={24}
+                      height={24}
+                      alt="closing order frame"
+                      className={styles.order_frame__cross}
+                      onClick={closeFrame}
+                      />
+                    </div>
+                    <h1 className={styles.order_frame__title}>{item.order_title}</h1>
+                    <p className={styles.order_frame__price}>От {item.price}c за 1шт.</p>
+                    <span className={styles.order_frame__description}>{item.order_text}</span>
+                    <span className={styles.order_frame__regions}>
+                      Место отправки: <b>{item.regions}</b>
+                    </span>
+                    <span  className={styles.order_frame__contact}>
+                      Контакты: <b>{item.phone_number}</b>
+                    </span>
+                    <button type="submit" className={styles.order_frame__frame_btn}>Написать поставщику</button>
+                  </div>
+                )))
+              }
               </div>
-            ))}
+            </div>
           </div>
         </div>
       </div>
